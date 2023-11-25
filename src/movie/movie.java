4fcb651f2163db4ad22ee7aa2200ecb6fcb227d2
Aykhan-05 <130337088+Aykhan-05@users.noreply.github.com>
2023-11-25 13:21:37 +0400

    Update movie.java

--- src/movie/movie.java
+++ src/movie/movie.java
@@ -1,50 +1,6 @@
 public class movie {
-    private String title;
-    private String director;
-    private int releaseYear;
-    private int runningTime;
 
     public static void main(String[] args){
        System.out.println("It will return movies!");
     }
-
-    public movie(String title, String director, int releaseYear, int runningTime) {
-        this.title = title;
-        this.director = director;
-        this.releaseYear = releaseYear;
-        this.runningTime = runningTime;
-    }
-    
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public int getReleaseYear() {
-        return releaseYear;
-    }
-
-    public void setReleaseYear(int releaseYear) {
-        this.releaseYear = releaseYear;
-    }
-
-    public int getRunningTime() {
-        return runningTime;
-    }
-
-    public void setRunningTime(int runningTime) {
-        this.runningTime = runningTime;
-    }
-
 }
