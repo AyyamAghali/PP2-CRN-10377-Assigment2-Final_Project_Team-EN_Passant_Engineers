f3190f25df8deb06330041fbd23c17a5c210e029
UserX <ayxanguliyev0513@gmail.com>
2023-12-14 23:48:34 +0400

    setsize

--- src/userGUI/MovieJFrame.java
+++ src/userGUI/MovieJFrame.java
@@ -7,6 +7,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.net.MalformedURLException;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -30,12 +34,31 @@ public class MovieJFrame {
     String path = "https://m.media-amazon.com/images/M/MV5BMzg4ZjQ4OGUtZjkxMi00Y2I2LWEzNTAtODI2ZjkxMGVjNTQwXkEyXkFqcGdeQXVyNjgxNTAwNjQ@._V1_.jpg";
     movie movie = new movie("The mask of zorro", "Martin Campbell", 90, 1998, path);
 
-    public static void main(String[] args) throws MalformedURLException {
+    /*static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost";
+
+    static final String USER = "root";
+    static final String PASS = "";/* */
+    public static void main(String[] args) throws MalformedURLException, Exception {
         MovieJFrame mJframe = new MovieJFrame();
+
+        /* Connection cnct = null;
+        Statement st = null;
+
+        Class.forName("JDBC_DRIVER");
+        
+        cnct = DriverManager.getConnection(DB_URL, USER, PASS);
+        st = cnct.createStatement();
+
+        String sql = "BUILD DATABASE";
+        st.executeUpdate(sql);
+        System.out.println("The database is being built");
+        st.close();
+        cnct.close();/* */
     }
 
     MovieJFrame() {
-        frame.setSize(1100, 1900);
+        frame.setSize(500, 500);
 
        // pan.setLayout(new GridLayout(3, 2, 5, 5));
 
@@ -63,6 +86,7 @@ public class MovieJFrame {
                     ImageIcon img;
                     try {
                         img = new ImageIcon(movie.getURL());
+                        frame.setSize(img.getIconWidth(), img.getIconHeight());
                         imageLabel.setIcon(img);
                     } catch (MalformedURLException e1) {
                          e1.printStackTrace();
